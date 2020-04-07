4bee270f99152b124c77a3a98973abf1df71b940
tegarma17 <63033415+tegarma17@users.noreply.github.com>
2020-04-07 12:01:35 +0700

    Add files via upload

---
+++ CRUD2/CRUD2/Form1.Designer.vb
@@ -0,0 +1,181 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.button1 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(178, 161)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(187, 22)
+        Me.TextBox4.TabIndex = 23
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(178, 113)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(187, 22)
+        Me.TextBox3.TabIndex = 22
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(178, 66)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(187, 22)
+        Me.TextBox2.TabIndex = 21
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(178, 17)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(187, 22)
+        Me.TextBox1.TabIndex = 20
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(36, 236)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(609, 202)
+        Me.DataGridView1.TabIndex = 19
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(551, 141)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(94, 42)
+        Me.Button3.TabIndex = 18
+        Me.Button3.Text = "DELETE"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(551, 79)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(94, 36)
+        Me.Button2.TabIndex = 17
+        Me.Button2.Text = "UBAH"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'button1
+        '
+        Me.button1.Location = New System.Drawing.Point(551, 16)
+        Me.button1.Name = "button1"
+        Me.button1.Size = New System.Drawing.Size(94, 42)
+        Me.button1.TabIndex = 16
+        Me.button1.Text = "TAMBAH"
+        Me.button1.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(58, 167)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(57, 17)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "HARGA"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(55, 119)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(54, 17)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "TOTAL"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(55, 66)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(109, 17)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "NAMA BARANG"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(55, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(83, 17)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "ID BARANG"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.button1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents button1 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+End Class
---
+++ CRUD2/CRUD2/Form1.vb
@@ -0,0 +1,108 @@
+﻿Imports System.Data.Odbc
+Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Dim cn As MySqlConnection
+    Dim ds As New DataSet
+    Dim dr As MySqlDataReader
+    Dim da As MySqlDataAdapter
+    Dim dt As DataTable
+    Dim cmd As MySqlCommand
+
+
+    Sub Koneksi()
+        cn = New MySqlConnection
+        cn.ConnectionString = "server=localhost;user=root;password=;database=barang;allow user variables=true"
+        cn.Open()
+
+    End Sub
+    Sub tampil()
+
+        Try
+            Koneksi()
+            da = New MySqlDataAdapter("SELECT * FROM barang", cn)
+            ds = New DataSet
+            da.Fill(ds, "barang")
+            DataGridView1.DataSource = ds.Tables("barang")
+            DataGridView1.ReadOnly = True
+        Catch ex As Exception
+            MsgBox("Menampilkan data gagal")
+        End Try
+    End Sub
+
+    Sub KosongData()
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+    End Sub
+    Private Sub button1_Click(sender As Object, e As EventArgs) Handles button1.Click
+        If TextBox1.Text = "" Or TextBox2.Text = "" Or TextBox3.Text = "" Or TextBox4.Text = "" Then
+            MsgBox("Harap isi")
+        Else
+            Koneksi()
+            Dim simpan As String = "insert into barang values('" & TextBox1.Text & "','" & TextBox2.Text & "','" & TextBox3.Text & "','" & TextBox4.Text & "')"
+            cmd = New MySqlCommand(simpan, cn)
+            cmd.ExecuteNonQuery()
+            MsgBox("Sukses")
+            tampil()
+            KosongData()
+
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        tampil()
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Koneksi()
+        Dim edit As String = "update barang set nama_barang='" & TextBox2.Text & "',total='" & TextBox3.Text & "', harga='" & TextBox4.Text & "'where id_barang='" & TextBox1.Text & "'"
+        cmd = New MySqlCommand(edit, cn)
+        cmd.ExecuteNonQuery()
+        MsgBox("DATA UPDATE")
+        tampil()
+        KosongData()
+
+    End Sub
+
+    Private Sub TextBox1_Keypress1(sender As Object, e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox1.KeyPress
+        TextBox1.MaxLength = 6
+        If e.KeyChar = Chr(13) Then
+            Koneksi()
+            cmd = New MySqlCommand("select * from barang where id_barang='" & TextBox1.Text & "'", cn)
+            dr = cmd.ExecuteReader
+            dr.Read()
+            If Not dr.HasRows Then
+                MsgBox("ID BARANG TIDAK ADA!, Silahkan Input lagi", "AHA")
+                TextBox1.Focus()
+            Else
+                TextBox2.Text = dr.Item("nama_barang")
+                TextBox3.Text = dr.Item("total")
+                TextBox4.Text = dr.Item("harga")
+                TextBox2.Focus()
+            End If
+
+        End If
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If TextBox1.Text = "" Then
+            MsgBox("silahkan pilih data yang akan di hapus")
+        Else
+            If MessageBox.Show("Yakin akan dihapus..?", "", MessageBoxButtons.YesNo) = Windows.Forms.DialogResult.Yes Then
+                Koneksi()
+                Dim hapus As String = "delete from barang where id_barang='" & TextBox1.Text & "'"
+                cmd = New MySqlCommand(hapus, cn)
+                cmd.ExecuteNonQuery()
+                tampil()
+                KosongData()
+
+            End If
+        End If
+    End Sub
+End Class
